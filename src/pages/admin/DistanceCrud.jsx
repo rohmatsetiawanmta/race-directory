fb25fc53adad1479d0f17a0eefb7724f22e1af02
rohmat <rohmat@ruangguru.com>
2025-10-18 19:24:17 +0700

    CRUD distance

--- src/pages/admin/DistanceCrud.jsx
+++ src/pages/admin/DistanceCrud.jsx
@@ -133,7 +133,7 @@ const DistanceModal = ({
           {/* Field: Jarak KM */}
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Jarak (KM)
+              Jarak (km)
             </label>
             <input
               type="number"
